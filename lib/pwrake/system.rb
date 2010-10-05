eae636530d1cf20faa8ca1270ecfef0be76614e4
Masahiro TANAKA <masa16.tanaka@gmail.com>
2010-10-05 10:35:31 +0900

    override backquote

--- lib/pwrake/system.rb
+++ lib/pwrake/system.rb
@@ -1,32 +1,59 @@
 require "pwrake/log"
 
+module Kernel
+  alias backquote :'`'
+  module_function :backquote
+end
+
+
 module FileUtils
   include Pwrake::Log
   alias rake_system_orig :rake_system
 
   def rake_system(*cmd)
     cmd_log = cmd.join(" ").inspect
-    start_time = Time.now
-    Pwrake.log("sh[start]:%s cmd=%s"%[Pwrake.time_str(start_time),cmd_log])
+    tm = Pwrake.timer("sh",cmd_log)
 
-    if conn = Thread.current[:connection]
+    conn = Thread.current[:connection]
+    if conn.kind_of?(Pwrake::Shell)
       res    = conn.system(*cmd)
       status = Rake::PseudoStatus.new(conn.status)
       status = Rake::PseudoStatus.new(1) if !res && status.nil?
     else
-      res    = Rake::AltSystem.system(*cmd)
+      res    = system(*cmd)
       status = $?
       status = Rake::PseudoStatus.new(1) if !res && status.nil?
     end
 
-    end_time = Time.now
-    elap_time = end_time - start_time
-    Pwrake.log("sh[end]:%s elap=%.3f status=%s cmd=%s"%
-        [Pwrake.time_str(end_time),elap_time,status.exitstatus,cmd_log])
+    tm.finish("status=%s cmd=%s"%[status.exitstatus,cmd_log])
     res
   end
-
 end
+
+module PwrakeFileUtils
+  module_function
+
+  def `(cmd) #`
+    cmd_log = cmd.inspect
+    tm = Pwrake.timer("bq",cmd_log)
+
+    conn = Thread.current[:connection]
+    if conn.kind_of?(Pwrake::Shell)
+      res    = conn.backquote(*cmd)
+      status = Rake::PseudoStatus.new(conn.status)
+      status = Rake::PseudoStatus.new(1) if !res && status.nil?
+    else
+      res    = Kernel.backquote(cmd)
+      status = $?
+      status = Rake::PseudoStatus.new(1) if !res && status.nil?
+    end
+
+    tm.finish("status=%s cmd=%s"%[status.exitstatus,cmd_log])
+    res
+  end
+end
+
+include PwrakeFileUtils
 
 if !defined? FileUtils::PseudoStatus
   FileUtils::PseudoStatus = Rake::PseudoStatus
